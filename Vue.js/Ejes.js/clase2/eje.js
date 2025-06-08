67c7e74c12375aac6051bf3d3dc4499573988d5b
Santiago Camarda <santi.camrda@gmail.com>
2025-06-08 11:44:35 -0300

    Agrega carpeta Vue.js con contenido inicial

---
+++ Vue.js/Ejes.js/clase2/eje.js
@@ -0,0 +1,13 @@
+const wes={
+    first:"Wes",
+    last:"Bos",
+    links:{
+        social:{
+            twiter:"https://twiter",
+            facebok:"htts:///faceboo",
+        }
+    }
+}
+
+const{twiter,facebok}=wes.links.social;
+console.log(`${twiter} and ${facebok}`);
